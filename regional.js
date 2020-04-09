5d4ee67b1af14cfda6c75ed201f0bf9198614efe
BiB <bogdan.bocioaca@gmail.com>
2020-04-09 13:57:47 +0300

    regional update

---
+++ regional.js
@@ -0,0 +1,17 @@
+export const regional = {
+	'ro': {
+		monthNames: ['Ian', 'Feb', 'Mar', 'Apr', 'Mai', 'Iun', 'Iul', 'Aug', 'Sept', 'Oct', 'Noi', 'Dec'],
+	    dayNames: ['Dum', 'Lun', 'Mar', 'Mie', 'Joi', 'Vin', 'Sam'],
+	    lang: 'ro'	
+	},
+	'en': {
+		monthNames: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+	    dayNames: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+	    lang: 'en'	
+	},
+	'nl' : {
+		monthNames: ['Jan', 'Feb', 'Mar', 'Apr', 'Mei', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec'],
+	    dayNames: ['Zon', 'Maa', 'Din', 'Woe', 'Don', 'Vrij', 'Zat'],
+	    lang: 'nl'	
+	}
+};
